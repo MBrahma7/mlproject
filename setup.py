e759c2e38c380400e5b74ced4d017c8b4fd59fdb
MBrahma7 <brahmaiah.my@gmail.com>
2026-02-14 13:47:32 -0500

    logging and excpetion

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPE_E_DOT ='-e .'
+
+def get_requirements(file_path)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [requirement.replace('\n',"") for requirement in requirements]
+        if HYPE_E_DOT in requirements:
+            requirements.remove(HYPE_E_DOT)   
+
+setup(
+    name= 'mlproject',
+    version='1.0.0',
+    author='Brahmaiah',
+    author_email='mbr217@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
