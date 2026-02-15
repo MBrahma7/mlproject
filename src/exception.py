1a6ec9a847b4751e17b83ffdcda54c989866c61f
MBrahma7 <brahmaiah.my@gmail.com>
2026-02-15 17:47:28 -0500

    Date Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tab= error_detail.exc_info()
@@ -12,6 +12,7 @@ class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        logging.error(self.error_message, exc_info=True)
     def __str__(self):
         return self.error_message
     
